1115d1ba81a3834b7432d9e279d1f92f85403d26
chwoerz <ch.woerz@gmail.com>
2019-06-01 23:48:41 +0200

    added nullable extension function example

--- src/extensions/StringUtils2.kt
+++ src/extensions/StringUtils2.kt
@@ -2,7 +2,7 @@ package extensions
 
 // 1
 fun String.prependHello(): String {
-    return "Hello " + this
+    return "Hello $this"
 }
 
 // 2
@@ -16,13 +16,25 @@ fun String.externalPartnerNr(): String {
     if (this.length != 10) {
         throw IllegalStateException("There is no way to create the external partnernr from this input")
     }
-    return "0-${this.substring(0..2)}0${this.substring(3..4)}-${this.substring(5)}"
+    return "0-${this.substring(0..1)}0${this[2]}-${this.substring(3)}"
+}
+
+fun String?.nullToString(): String {
+    return if (this.isNullOrBlank()) "null" else this
 }
 
 fun JavaLibrary.doubleText(): String {
-    return this.text + this.text
+    return "$text $text"
 }
 
 fun main() {
-
+    println("Techtalk".prependHello()) // "Hello Techtalk
+    println("1a2b3c".countNumbers()) // 3
+    println("0123456789".externalPartnerNr()) // 0-0102-3456789
+    var s: String? = null
+    println(s.nullToString()) // "null"
+    s = "Hi Techtalk"
+    println(s.nullToString()) // "Techtalk"
+    val javaLibrary = JavaLibrary("text")
+    println(javaLibrary.doubleText()) // "text text"
 }
